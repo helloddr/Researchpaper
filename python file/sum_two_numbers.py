f456fb02b45c2487ad35b1a6051bd2799daf26ce
helloddr <dharanidharreddy999@gmail.com>
2026-03-11 23:08:15 -0500

    Remove sum of two numbers script

--- python file/sum_two_numbers.py
+++
@@ -1,8 +0,0 @@
-def add_numbers(a: float, b: float) -> float:
-    return a + b
-
-
-if __name__ == "__main__":
-    first = float(input("Enter first number: "))
-    second = float(input("Enter second number: "))
-    print("Sum:", add_numbers(first, second))
